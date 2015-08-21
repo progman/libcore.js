f579cd41696c723a5a4e6117fc1365e1b9af16ae
progman <gnuplanet@gmail.com>
2015-08-22 01:16:46 +0300

    add libcore__jdraw()

--- libcore.js
+++ libcore.js
@@ -1,5 +1,5 @@
 //---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
-// 0.2.1
+// 0.2.2
 // Alexey Potehin <gnuplanet@gmail.com>, http://www.gnuplanet.ru/doc/cv
 //---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
 var libcore =
@@ -23,7 +23,8 @@ var libcore =
 	"array_expand"  : libcore__array_expand,
 	"array_limit"   : libcore__array_limit,
 	"array_remix"   : libcore__array_remix,
-	"array_filter"  : libcore__array_filter
+	"array_filter"  : libcore__array_filter,
+	"jdraw"         : libcore__jdraw
 };
 //---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
 /**
@@ -750,3 +751,232 @@ function libcore__array_filter(source, filter, flag_invert, flag_identity)
 	return target;
 }
 //---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
+/**
+ * draw json pretty
+ * \param[in] source variable for print
+ * \param[in] source_field variable name (not not required)
+ * \param[in] flag_shift flag for more pretty view (not not required)
+ * \param[in] tab inner variable
+ * \param[in] max_width inner variable
+ * \return result array
+ */
+function libcore__jdraw(source, source_field, flag_shift, tab, max_width)
+{
+	function shift(flag_shift, cur, max)
+	{
+		if (flag_shift !== true) return ' ';
+
+		var shift = '';
+		for (var i=0; i < (max - cur + 1); i++)
+		{
+			shift += ' ';
+		}
+		return shift;
+	}
+
+	if (typeof source_field === 'undefined')
+	{
+		source_field = '';
+	}
+
+	if (typeof flag_shift === 'undefined')
+	{
+		flag_shift = false;
+	}
+
+	if (typeof tab === 'undefined')
+	{
+		tab = '';
+	}
+
+	if (typeof max_width === 'undefined')
+	{
+		max_width = 0;
+	}
+
+	if (typeof source === 'boolean')
+	{
+		var body = '';
+		if (source_field != '')
+		{
+			body += tab + '"' + source_field + '"' + shift(flag_shift, source_field.length, max_width) + ': ' + source;
+		}
+		else
+		{
+			body += tab + source;
+		}
+		return body;
+	}
+
+	if (typeof source === 'number')
+	{
+		var body = '';
+		if (source_field != '')
+		{
+			body += tab + '"' + source_field + '"' + shift(flag_shift, source_field.length, max_width) + ': ' + source;
+		}
+		else
+		{
+			body += tab + source;
+		}
+		return body;
+	}
+
+	if (typeof source === 'string')
+	{
+		var body = '';
+		if (source_field != '')
+		{
+			body += tab + '"' + source_field + '"' + shift(flag_shift, source_field.length, max_width) + ': "' + source + '"';
+		}
+		else
+		{
+			body += tab + '"' + source + '"';
+		}
+		return body;
+	}
+
+	if (source === null)
+	{
+		var body = '';
+		if (source_field != '')
+		{
+			body += tab + '"' + source_field + '"' + shift(flag_shift, source_field.length, max_width) + ': ' + source;
+		}
+		else
+		{
+			body += tab + source;
+		}
+		return body;
+	}
+
+	if (typeof source === 'undefined')
+	{
+		var body = '';
+		if (source_field != '')
+		{
+			body += tab + '"' + source_field + '"' + shift(flag_shift, source_field.length, max_width) + ': ' + source;
+		}
+		else
+		{
+			body += tab + source;
+		}
+		return body;
+	}
+
+	if (typeof source === 'function')
+	{
+		var body = '';
+		if (source_field != '')
+		{
+			body += tab + '"' + source_field + '"' + shift(flag_shift, source_field.length, max_width) + ': ' + source;
+		}
+		else
+		{
+			body += tab + source;
+		}
+		return body;
+	}
+
+	if ((source instanceof RegExp) === true)
+	{
+		var body = '';
+		if (source_field != '')
+		{
+			body += tab + '"' + source_field + '"' + shift(flag_shift, source_field.length, max_width) + ': ' + source;
+		}
+		else
+		{
+			body += tab + source;
+		}
+		return body;
+	}
+
+	if ((source instanceof Date) === true)
+	{
+		var body = '';
+		if (source_field != '')
+		{
+			body += tab + '"' + source_field + '"' + shift(flag_shift, source_field.length, max_width) + ': ' + source;
+		}
+		else
+		{
+			body += tab + source;
+		}
+		return body;
+	}
+
+	if ((source instanceof Array) === true)
+	{
+		var body = '';
+		if (source_field != '')
+		{
+			body += tab + '"' + source_field + '"' + shift(flag_shift, source_field.length, max_width) + ':' + "\n";
+		}
+		body += tab + "[\n";
+
+		for (var i=0; i < source.length; i++)
+		{
+			body += jprint(source[i], undefined, flag_shift, tab + "\t");
+
+			if (i != (source.length - 1))
+			{
+				body += ",\n";
+			}
+			else
+			{
+				body += "\n";
+			}
+		}
+
+		body += tab + "]";
+		return body;
+	}
+
+	if (typeof source === 'object')
+	{
+		var body = '';
+		if (source_field != '')
+		{
+			body += tab + '"' + source_field + '"' + shift(flag_shift, source_field.length, max_width) + ':' + "\n";
+		}
+		body += tab + "{\n";
+
+		var max_width = 0;
+		var field_list = [];
+		for (source_field in source)
+		{
+			field_list.push(source_field);
+			if (source_field.length > max_width)
+			{
+				max_width = source_field.length;
+			}
+		}
+
+		for (var i=0; i < field_list.length; i++)
+		{
+			source_field = field_list[i];
+
+			body += jprint(source[source_field], source_field, flag_shift, tab + "\t", max_width);
+
+			if (i != (field_list.length - 1))
+			{
+				body += ",\n";
+			}
+			else
+			{
+				body += "\n";
+			}
+		}
+
+		body += tab + "}";
+
+		return body;
+	}
+
+	var body = '';
+	body += 'ERROR[libcore.jprint()]: not implementation type ' + typeof source;
+
+	return body;
+}
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
